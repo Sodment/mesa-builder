69c32ae2d4d28086cbbe0302936f34aabd9c4c97
Koch, Pawel <pawel.koch@intel.com>
2026-07-01 10:08:24 +0200

    Add flag for usage

--- kernel-build.sh
+++ kernel-build.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+USE_LOCALMODCONFIG=0
+[ "$1" = "--localmodconfig" ] && USE_LOCALMODCONFIG=1
+
 git clean -fxd
 cp /boot/config-`uname -r` ./.config
 
-yes '' | make localmodconfig
+if [ "$USE_LOCALMODCONFIG" -eq 1 ]; then
+  yes '' | make localmodconfig
+fi
 
 # remove trusted keys
 scripts/config --disable SYSTEM_REVOCATION_KEYS
@@ -19,5 +24,5 @@ scripts/config --undefine DEBUG_INFO_COMPRESSED
 scripts/config --set-val  DEBUG_INFO_NONE       y
 scripts/config --set-val  DEBUG_INFO_DWARF5     n
 
+# --- build ----------------------------------------------------------------
 yes '' | make oldconfig && make clean && make -j `getconf _NPROCESSORS_ONLN` deb-pkg LOCALVERSION=-`git describe --tags --always | sed 's#/#_#g' | sed 's#_#-#g' | tr '[:upper:]' '[:lower:]'`
-
